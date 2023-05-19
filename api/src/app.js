40a91c80a209cbd757fa16cb364dbf0afd8044bc
Yonner-suarez <yonnerhazziel@gmail.com>
2023-05-18 19:18:43 -0500

    third commit

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "http://localhost:5173"
+    "*"
     //"https://app-pokedex-three.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
