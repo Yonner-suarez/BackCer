87a7f52997295e6a8b7f4eebed695a4bf6362de8
Yonner-suarezLilab <yonner.suarez@lilab.pe>
2023-07-02 09:17:46 -0500

    payment methods

--- api/src/controllers/generarOrden.js
+++ api/src/controllers/generarOrden.js
@@ -1,5 +1,4 @@
 const mercadopago = require("mercadopago");
-
 mercadopago.configure({
   access_token:
     "TEST-337264305447969-052614-1a8550f94f6f169b0947602117976077-1383343081",
@@ -22,8 +21,31 @@ const generarOrden = async (producto) => {
       failure: "https://page-cer.vercel.app/failure",
     },
     notification_url: "https://backcer.onrender.com/confirmacionDePago",
+    // back_urls: {
+    //   success: "http://localhost:5173/success",
+    //   pending: "http://localhost:5173/pending",
+    //   failure: "http://localhost:5173/failure",
+    // },
+    // notification_url:
+    //   "https://4d80-2803-1800-1118-1d47-cc5c-ba29-a135-93a4.ngrok.io/confirmacionDePago",
+
+    payment_methods: {
+      excluded_payment_methods: [
+        {
+          id: "efecty",
+        },
+      ],
+      excluded_payment_types: [
+        {
+          id: "cash",
+        },
+        {
+          id: "efecty",
+        },
+      ],
+      installments: 1,
+    },
     binary_mode: true,
-    auto_return: "approved",
     expires: true,
     installments: 1,
   };
@@ -37,3 +59,22 @@ const generarOrden = async (producto) => {
 };
 
 module.exports = generarOrden;
+
+//const headers = {
+//   Authorization: `Bearer TEST-337264305447969-052614-1a8550f94f6f169b0947602117976077-1383343081`,
+// };
+// // Realiza una solicitud GET a la API de Mercado Pago para obtener los métodos de pago
+// axios
+//   .get("https://api.mercadopago.com/v1/payment_methods", { headers })
+//   .then((response) => {
+//     const paymentMethods = response.data;
+
+//     // Muestra los métodos de pago en la consola
+//     console.log("Métodos de pago disponibles en Mercado Pago:");
+//     paymentMethods.forEach((method) => {
+//       console.log(`- ${method.id}: ${method.name}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error al obtener los métodos de pago:", error);
+//   });
